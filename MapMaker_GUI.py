5c6013e97ae1de9fe29e7979ad7acb53346daaea
Turidus <31318547+Turidus@users.noreply.github.com>
2018-07-19 18:51:35 +0200

    added core functionality
    Made the GUI better

--- MapMaker_GUI.py
+++ MapMaker_GUI.py
@@ -20,11 +20,40 @@ class Args():
     
 arguments = Args()
 
+def _dispatchMapMaker(args):
+    
+    errorHappend = False
+    errorString = ""
+    
+    try:
+        MapMaker.MapMaker(arguments)
+        
+    except IOError as ioErr:
+        errorHappend = True
+        errorString += str(ioErr) + "\n"
+    
+    except ValueError as vErr:
+        errorHappend = True
+        errorString += str(vErr) + "\n"
+        
+    finally:
+        if errorHappend:
+            print(errorString)
+            app.queueFunction(app.setMessage, "output", errorString + "\nStatus: Waiting\n")
+        else:
+            app.queueFunction(app.setMessage,"output", "Status: Done\n\nStatus: Waiting\n")
+            
+        app.queueFunction(app.enableButton,"Go")
+    
+    
+
 def press(name):
     
     if name == "Exit":
         app.stop()
-        
+    
+    app.disableButton("Go")
+    
     errorHappend = False
     errorString = ""
         
@@ -91,6 +120,7 @@ def press(name):
         
     if errorHappend:
         app.setMessage("output", errorString + "Status: Waiting\n")
+        app.enableButton("Go")
         return
     
     arguments.twoD = boxes["2D"]
@@ -103,29 +133,15 @@ def press(name):
     
     arguments.s = boxes["Schematic"]
     
-    try:
-        app.setMessage("output", "Status: Running. This can take awhile\n")
-        MapMaker.MapMaker(arguments)
-        
-    except IOError as ioErr:
-        errorHappend = True
-        errorString += str(ioErr) + "\n"
     
-    except ValueError as vErr:
-        errorHappend = True
-        errorString += str(vErr) + "\n"
-        
-    finally:
-        if errorHappend:
-            print(errorString)
-            app.setMessage("output", errorString + "\nStatus: Waiting\n")
-        else:
-            app.setMessage("output", "Status: Done\n\nStatus: Waiting\n")
-        
-        
-        
-        
-        
+    app.setMessage("output", "Status: Running. This can take awhile\n")
+    
+    try:
+        app.thread(_dispatchMapMaker,arguments)
+    except AttributeError as atErr:
+        app.setMessage("output","Error: " + str(atErr) + "\nStatus: Waiting\n")
+        print(str(atErr))
+    
 
 
 #---Building the GUI----
@@ -133,21 +149,29 @@ app = gui("Minecraft Map Maker")
 app.setSize("1000x600")
 
 #---Column 0
-app.setSticky("we")
-app.addLabel("pathToImage","Path to image:",row = 1, column = 0)
-app.addLabel("bl","Base color IDs that\nshould not be used:",row = 2, column = 0)
-app.addLabel("n","Optional name:",row = 3, column = 0)
+app.setSticky("w")
+app.addLabel("pathToImage","Path to image",row = 1, column = 0)
+app.setLabelAlign("pathToImage","e")
+
+app.addLabel("bl","Base color IDs that\nshould not be used",row = 2, column = 0)
+app.setLabelAlign("bl","e")
+
+app.addLabel("n","Name",row = 3, column = 0)
+app.setLabelAlign("n","e")
 
 #---Column 1
-app.setSticky("we")
+app.setSticky("w")
 
 app.addFileEntry("pathToImage",row = 1, column = 1)
-app.setEntry("pathToImage", "Path to the image")
+#app.setEntry("pathToImage", "Path to the image")
+app.setEntryTooltip("pathToImage", "The path to your image")
 
 app.addEntry("bl", row = 2, column = 1)
-app.setEntry("bl", "For example: 33, 14, 5")
+app.setEntryTooltip("bl", "For example: 33, 14, 5\nSee Readme for more infos")
+#app.setEntry("bl", "For example: 33, 14, 5")
 
 app.addEntry("n", row = 3, column = 1)
+app.setEntryTooltip("n", "optional")
 
 
 #---Column 2
@@ -162,18 +186,24 @@ app.addVerticalSeparator(row = 0, column = 2, rowspan = 6)
 app.setSticky("w")
 app.addLabel("switches","Switches", row = 0, column = 3)
 app.addCheckBox("2D", row = 1, column = 3)
+app.setCheckBoxTooltip("2D", "Generate a 2D instead of a stepped construced. Only 53 colors.")
 
 app.addCheckBox("Picture", row = 2, column = 3)
 app.setCheckBox("Picture")
+app.setCheckBoxTooltip("Picture", "Generate a preview picture")
 
 app.addCheckBox("Block positions", row = 3, column = 3)
 app.setCheckBox("Block positions")
+app.setCheckBoxTooltip("Block positions", "Generate a txt file with all blocks an their positions")
+
 
 app.addCheckBox("Block amount", row = 4, column = 3)
 app.setCheckBox("Block amount")
+app.setCheckBoxTooltip("Block amount", "Generate a txt file with all blocks and the amount needed")
 
 app.addCheckBox("Schematic", row = 5, column = 3)
 app.setCheckBox("Schematic")
+app.setCheckBoxTooltip("Schematic", "Generate a schematic file for compatible plugins")
 
 
 #---Column 4
@@ -182,35 +212,51 @@ app.addVerticalSeparator(row = 0, column = 4, rowspan = 6)
 app.setPadding(0)
 
 #---Column 5
-app.setSticky("ew")
-app.addLabel("minY","Minimal Y coordiante:",row = 1, column = 5)
-app.addLabel("maxY","Maximum Y coordiante:",row = 2, column = 5)
-app.addLabel("maxS","Maxium Schematic size:",row = 3, column = 5)
+app.setSticky("w")
+app.addLabel("ints","Dimensions", row = 0, column = 5)
+app.addLabel("minY","Minimal Y coordiante",row = 1, column = 5)
+app.setLabelAlign("minY","e")
+app.addLabel("maxY","Maximum Y coordiante",row = 2, column = 5)
+app.setLabelAlign("maxY","e")
+app.addLabel("maxS","Maxium Schematic size",row = 3, column = 5)
+app.setLabelAlign("maxS","e")
 
 #---Column 6
-app.setSticky("ew")
-app.addLabel("ints","Dimensions", row = 0, column = 6)
+app.setSticky("w")
+
 app.addValidationEntry("minY", row = 1, column = 6)
 app.setEntry("minY", "4")
+app.setEntryMaxLength("minY", 3)
+app.setEntryWidth("minY", 5)
+app.setEntryTooltip("minY", "At least 0, at most 251. Needs to be 4 smaller than maxY")
 
 app.addValidationEntry("maxY", row = 2, column = 6)
 app.setEntry("maxY", "250")
+app.setEntryMaxLength("maxY", 3)
+app.setEntryWidth("maxY", 5)
+app.setEntryTooltip("maxY", "At least 4, at most 255. Needs to be 4 bigger than minY")
 
 app.addValidationEntry("maxS", row = 3, column = 6)
 app.setEntry("maxS", "129")
+app.setEntryMaxLength("maxS", 5)
+app.setEntryWidth("maxS", 5)
+app.setEntryTooltip("maxS", "At least 1. The bigger maxS becomes, the bigger the impact of pasting the schematic")
 
 
 #---Row 7
-app.setSticky("w")
+app.setSticky("we")
 app.addMessage("output", "Status: Waiting\n", row = 6, column = 0, colspan = 7)
 #app.setMessageAspect("output", 1000)
 app.setMessageWidth("output", 1000)
+#app.setMessageHeight("output", 100)
 app.setMessageBg("output", "white")
+app.setMessageRelief("output", "solid")
+app.setMessageAlign("output", "w")
 
 #---Row 8
+app.setSticky("we")
 app.addButton("Go", press, row = 7, column = 0)
-app.addButton("Exit", press, row = 7, column = 4) 
-
+app.addButton("Exit", press, row = 7, column = 5)
 
 
 
